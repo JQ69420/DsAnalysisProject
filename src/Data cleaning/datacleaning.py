a5b432dbcca1c1f42e13af51988912538ed35c95
JQ69420 <1035208@hr.nl>
2024-10-06 20:54:01 +0200

    merged anl2 attandace with student files

--- src/Data cleaning/datacleaning.py
+++ src/Data cleaning/datacleaning.py
@@ -34,4 +34,55 @@ def Drop_Duplicated():
     df_students_unique.to_excel(output_file, index=False)
     print(f"Unique rows saved to: {output_file}")
 
-Drop_Duplicated()
+def Merge_Files_ANL2_Attandace():
+    attandace_file = os.path.join("..", "..", "data", "raw", "ANL2-2020-2021 Homework and Attendance.xlsx")
+    student_file = os.path.join("..", "..", "data", "processed", "combined_students.xlsx")
+
+    df_students = pd.read_excel(student_file)
+    df_attandace = pd.read_excel(attandace_file)
+
+    df_students['id'] = df_students['id'].fillna(0).astype(int)
+    df_attandace['id'] = df_attandace['id'].fillna(0).astype(int)  
+
+    df_students['id'] = df_students['id'].astype(str).str.strip()
+    df_attandace['id'] = df_attandace['id'].astype(str).str.strip()
+
+    print(df_attandace.head)
+    merged_data = pd.merge(df_students, df_attandace[['id', 'Attendance %', "Team assignment"]], on='id', how='left')
+    merged_data.rename(columns={'Attendance %': 'Attandace ANL2', 'Team assignment': 'Team Assignment ANL2'}, inplace=True)  # Change column name
+    column_order = list(df_students.columns) + ['Attandace ANL2' ,  'Team Assignment ANL2'] 
+    merged_data = merged_data[column_order]
+
+    output_file = os.path.join("..", "..", "data", "processed", "Merged_Attandace_ANL2.xlsx")
+    merged_data.to_excel(output_file, index=False)
+
+
+
+def Merge_Files_ANL1_Attandace(): # werkt nog niet
+    attandace_file = os.path.join("..", "..", "data", "raw", "ANL1-2020-2021 attendance.xlsx")
+    student_file = os.path.join("..", "..", "data", "processed", "Merged_Attandace_ANL2.xlsx")
+
+    df_students = pd.read_excel(student_file)
+    df_attandace = pd.read_excel(attandace_file)
+
+    df_students['id'] = df_students['id'].fillna(0).astype(int)
+    df_attandace['id'] = df_attandace['id'].fillna(0).astype(int)  
+
+    df_students['id'] = df_students['id'].astype(str).str.strip()
+    df_attandace['id'] = df_attandace['id'].astype(str).str.strip()
+
+    for s in df_students['id']:
+        for s1 ,s2 in zip(df_attandace['id'], df_attandace['Attendance %']):
+            if(s == s1):
+                print("TRUE")
+                print(s2)
+    print(df_attandace.head)
+    merged_data = pd.merge(df_students, df_attandace[['id', 'Attendance %']], on='id', how='left')
+    merged_data.rename(columns={'Attendance %': 'Attandace ANL1'}, inplace=True)  # Change column name
+    column_order = list(df_students.columns) + ['Attandace ANL1'] 
+    merged_data = merged_data[column_order]
+
+    output_file = os.path.join("..", "..", "data", "processed", "Merged_students_ANL1+ANL2.xlsx")
+    merged_data.to_excel(output_file, index=False)
+
+Merge_Files_ANL2_Attandace()
